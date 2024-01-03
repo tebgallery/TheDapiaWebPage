1a1376ffe6d9b7e6469dc9d43ed0e5b9506abaed
Esteban <estebansanchez2013@gmail.com>
2024-01-03 01:51:08 -0300

    se agrega seccion de login

---
+++ thedapia-webpage/src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "./LoginForm";
+import './Login.css';
+
+const Login = () => {
+  return (
+    <section class = "w-screen h-screen flex h-screen bg-white">
+      <LoginForm/>
+    </section>
+  );
+};
+
+export default Login;
