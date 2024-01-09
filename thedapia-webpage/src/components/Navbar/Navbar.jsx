1a34007df218b9a6629069003fd4258d2f97e5f8
Esteban <estebansanchez2013@gmail.com>
2024-01-09 01:13:15 -0300

    Modificacion de navbar

--- thedapia-webpage/src/components/Navbar/Navbar.jsx
+++ thedapia-webpage/src/components/Navbar/Navbar.jsx
@@ -1,45 +1,52 @@
-import Logo from '../../img/thedapia-logo.png'
+import Logo from "../../img/thedapia-logo.png";
 
 const Navbar = () => {
   return (
-    <nav class="flex sticky w-screen h-36">
-        <div class="absolute top-0 flex w-screen h-24 bg-fuchsia-400 justify-between">
-            <div class="w-16 h-16 m-auto">
-                <img src={Logo}  alt="" />
-            </div>
-
-            <div class="w-1/2 h-14 bg-black m-auto relative mb-4 flex w-full flex-wrap items-stretch">
-                <input
-                            type="search"
-                            className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                            placeholder="Buscar productos.."
-                            aria-label="Search"
-                            aria-describedby="button-addon1" />
-                <button
-                        className="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
-                        type="button"
-                        id="button-addon1">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-5 w-5">
-                            <path
-                                fillRule="evenodd"
-                                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                                clipRule="evenodd" />
-                        </svg>
-                    </button>
-            </div>
-
-            <div class="w-20 h-16 bg-amber-200 m-auto">
-            </div>
-
-
+    <nav class="flex sticky w-screen h-36 top-0">
+      <div class="absolute top-0 flex w-screen h-24 bg-fuchsia-400 justify-between border-b-2">
+        <div class="w-16 h-16 m-auto">
+          <img src={Logo} alt="" />
         </div>
-        <div class="absolute bottom-0 flex w-screen h-12 bg-emerald-400">
-            
+
+        <div class="w-1/2 h-12 m-auto relative flex">
+          <input
+            type="search"
+            className="w-full pl-4 rounded-l-2xl outline-none"
+            placeholder="Buscar productos..."
+          />
+          <button
+            class="w-16 rounded-r-2xl bg-white flex items-center justify-center"
+            type="button"
+          >
+            <svg class="w-7 h-7" viewBox="0 0 1000 1000">
+              <path
+                class="path1"
+                d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 
+                                            55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 
+                                            140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 
+                                            79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 
+                                            218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"
+              ></path>
+            </svg>
+          </button>
         </div>
+
+        <div class="w-44 h-16 bg-amber-200 m-auto flex items-center justify-around ">
+          <div class="w-14 h-14 bg-black  "></div>
+
+          <div class="w-14 h-14 bg-white  "></div>
+        </div>
+      </div>
+      <div class="absolute bottom-0 flex w-screen h-12 bg-emerald-400 flex items-center justify-center ">
+        <ul class= "space-x-14">
+            <li class="inline-block"> <a href="" class="text-lg text-black  hover:text-white">Inicio</a> </li>
+            <li class="inline-block"> <a href="" class="text-lg text-black  hover:text-white">Catalogo</a> </li>
+            <li class="inline-block"> <a href="" class="text-lg text-black  hover:text-white">Juguetes</a> </li>
+            <li class="inline-block"> <a href="" class="text-lg text-black  hover:text-white">Contacto</a> </li>
+            <li class="inline-block"> <a href="" class="text-lg text-black  hover:text-white">Nosotros</a> </li>
+        </ul>
+
+      </div>
     </nav>
   );
 };
