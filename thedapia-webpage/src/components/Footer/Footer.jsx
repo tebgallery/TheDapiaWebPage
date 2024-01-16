1ba7fe30c595d2505c77217ff359e8b7ab490f0b
Esteban <estebansanchez2013@gmail.com>
2024-01-16 02:17:19 -0300

    se agrega footer y cambios en secciones

--- thedapia-webpage/src/components/Footer/Footer.jsx
+++ thedapia-webpage/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4">
       <div className="container mx-auto">
-        <p className="text-center">© The Dapia Sitio Web 2024. Todos los derechos reservados.</p>
+        <p className="text-center"> © The Dapia Sitio Web 2024. Todos los derechos reservados.</p>
       </div>
     </footer>
   );
